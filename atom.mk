43f0e81cdb547f976a26898d66d173980016a5ea
Maxime MAITRE <maxime.maitre@parrot.com>
2016-05-03 15:52:46 +0200

    [DEBUG] fix arsdkgen macro
    Change-Id: Ic0cef2d1e467e3b6d01aedc1403d700925fa46b8
    Reviewed-on: https://gerrit.parrot.biz/37449
    Reviewed-by: Jean-Baptiste Dubois <jean-baptiste.dubois@parrot.com>

--- atom.mk
+++ atom.mk
@@ -93,6 +93,7 @@ $$(arsdkgen_done_file): .FORCE
 # Update alchemy variables for the module
 LOCAL_CLEAN_FILES += $$(arsdkgen_done_file) $(if $(call is-path-absolute,$2),$(empty),$$(arsdkgen_gen_files))
 LOCAL_EXPORT_PREREQUISITES += $$(arsdkgen_gen_files)
+LOCAL_PREREQUISITES += $$(arsdkgen_gen_files)
 LOCAL_CUSTOM_TARGETS += $$(arsdkgen_done_file)
 LOCAL_DEPENDS_HOST_MODULES += host.arsdkgen
 LOCAL_C_INCLUDES += $$(arsdkgen_out_dir)
